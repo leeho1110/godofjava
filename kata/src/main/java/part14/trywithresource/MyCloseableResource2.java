992554c07bb30815b25229bf889ed47338a2bed5
holee <ho.sol.lee@gmail.com>
2022-08-15 14:28:13 +0900

    feat: Part 14, '예외' try-with-resource 카타 코드 수정

--- kata/src/main/java/part14/trywithresource/MyCloseableResource2.java
+++ kata/src/main/java/part14/trywithresource/MyCloseableResource2.java
@@ -1,6 +1,6 @@
 package part14.trywithresource;
 
-public class MyCloseableResource implements AutoCloseable {
+public class MyCloseableResource2 implements AutoCloseable {
 
     public void doSomething(){
         System.out.println("invoke " + this.getClass().getSimpleName() + ".doSomething()");
--- kata/src/main/java/part14/trywithresource/TryWithResourceSample.java
+++ kata/src/main/java/part14/trywithresource/TryWithResourceSample.java
@@ -7,6 +7,25 @@ import java.io.IOException;
 public class TryWithResourceSample {
     public static void main(String[] args) throws IOException {
         pracWithBufferedInputStream();
+        pracWithAutoCloseableResource();
+    }
+
+    private static void pracWithAutoCloseableResource() {
+        try(
+                MyCloseableResource resource = new MyCloseableResource();
+                MyCloseableResource2 resource2 = new MyCloseableResource2();
+
+        ){
+            resource.doSomething(); // invoke MyCloseableResource.doSomething()
+            resource2.doSomething();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        // invoke MyCloseableResource2.close()
+        // invoke MyCloseableResource.close() -> 나중에 선언된 순대로 반환되며, try 구문을 빠져나오면서 바로 반환
+
+        System.out.println("Closeable instances are all closed?"); // Closeable instances are all closed?
     }
 
     private static void pracWithBufferedInputStream() {
